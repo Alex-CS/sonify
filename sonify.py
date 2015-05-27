833d3953bc8560568019e614556455951154eb7c
Alex Simonides <alex.simonides@redbeacon.com>
2015-05-27 02:21:14 -0500

    Add global constants

--- sonify.py
+++ sonify.py
@@ -7,32 +7,27 @@
 
 from library.music import *
 
-# Buttons / Joysticks
-trigger_right = RT = "trigger_right"
-trigger_left = LT = "trigger_left"
-bumper_right = RB = "bumper_right"
-bumper_left = LB = "bumper_left"
-d_pad_up = DPU = "d_pad_up"
-d_pad_down = DPD = "d_pad_down"
-d_pad_left = DPL = "d_pad_left"
-d_pad_right = DPR = "d_pad_right"
-button_a = A = "button_a"
-button_b = B = "button_b"
-button_x = X = "button_x"
-button_y = Y = "button_y"
-analog_stick_right = RAS = "analog_right"
-analog_stick_left = LAS = "analog_left"
+# Triggers / Joysticks
+trigger_left = LT = "bLeftTrigger"
+trigger_right = RT = "bRightTrigger"
+analog_stick_left_x = LAX = "sThumbLX"
+analog_stick_left_y = LAY = "sThumbLY"
+analog_stick_right_x = RAX = "sThumbRX"
+analog_stick_right_y = RAY = "sThumbRY"
 
-presses = {
-    RB: None,
-    LB: None,
-    A: None,
-    B: None,
-    X: None,
-    Y: None,
-}
+# Buttons
+d_pad_up = DPU = "0x0001"
+d_pad_down = DPD = "0x0002"
+d_pad_left = DPL = "0x0004"
+d_pad_right = DPR = "0x0008"
+bumper_left = LB = "0x0100"
+bumper_right = RB = "0x0200"
+button_a = A = "0x1000"
+button_b = B = "0x2000"
+button_x = X = "0x4000"
+button_y = Y = "0x8000"
 
-parts = [
+instruments = [
     Part(ACOUSTIC_BASS, 0),
     Part(STRING_ENSEMBLE2, 1),
     Part(ORCHESTRA_HIT, 2),
@@ -41,8 +36,15 @@ parts = [
     Part(TREMOLO_STRINGS, 5),
 ]
 
-
-
+# Map each button to a list of notes
+note_lists = {
+    RB: [0, 2, 0, 3, 0, 4, 0, 5],
+    LB: [7, 5, 7, 4, 7, 3, 7, 2],
+    A: [5, 4, 3, 2, 4, -2, 1, 0],
+    B: [7, 5, 6, 4, 5, 3, 2, 4],
+    X: [2, 1, 0, 3, 4, 5, 6, 7],
+    Y: [2, 1, 3, 2, 4, 3, 6, 7],
+}
 
 
 def get_controller_input(fname):
