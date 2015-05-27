5de7de222a5f5c8a329fb402859e14939f350df4
Alex Simonides <alex.simonides@redbeacon.com>
2015-05-27 06:06:21 -0500

    Build parser for log

--- sonify.py
+++ sonify.py
@@ -5,7 +5,9 @@
 #
 # Author: Alex Simonides
 
-from library.music import *
+import re
+
+# from library.music import *
 
 # Triggers / Joysticks
 trigger_left = LT = "bLeftTrigger"
@@ -14,6 +16,8 @@ analog_stick_left_x = LAX = "sThumbLX"
 analog_stick_left_y = LAY = "sThumbLY"
 analog_stick_right_x = RAX = "sThumbRX"
 analog_stick_right_y = RAY = "sThumbRY"
+LA = "sThumbL"
+RA = "sThumbR"
 
 # Buttons
 d_pad_up = DPU = "0x0001"
@@ -27,14 +31,14 @@ button_b = B = "0x2000"
 button_x = X = "0x4000"
 button_y = Y = "0x8000"
 
-instruments = [
-    Part(ACOUSTIC_BASS, 0),
-    Part(STRING_ENSEMBLE2, 1),
-    Part(ORCHESTRA_HIT, 2),
-    Part(OBOE, 3),
-    Part(TIMPANI, 4),
-    Part(TREMOLO_STRINGS, 5),
-]
+# instruments = [
+#     Part(ACOUSTIC_BASS, 0),
+#     Part(STRING_ENSEMBLE2, 1),
+#     Part(ORCHESTRA_HIT, 2),
+#     Part(OBOE, 3),
+#     Part(TIMPANI, 4),
+#     Part(TREMOLO_STRINGS, 5),
+# ]
 
 # Map each button to a list of notes
 note_lists = {
@@ -46,24 +50,71 @@ note_lists = {
     Y: [2, 1, 3, 2, 4, 3, 6, 7],
 }
 
+# ROOT = C4
 
-def get_controller_input(fname):
+def parse_time(time_string):
+    # TODO fix this once we know the time format
+    return time_string
+
+def parse_controller_input(fname):
+    """ Reads a log file and returns a list of dictionary samples
+    that each look something like this:
+    {
+        "time": string,
+        "buttons": list,
+        LT: int,
+        RT: int,
+        LA: tuple,
+        RA: tuple
+    }
+    """
+
+    # RegEx setup
+    base_pattern = r"%s: %s;"
+    catchall = r"([^; ]+)"
+    delimiter = '\n"$";\n'
+
+    def get_first_match(match_obj):
+        """ Safely retrieve the first match. """
+        return match_obj.group(1) if match_obj else None
+
+    # Read in the file and split it into sample chunks
     input_ = open(fname, "r")
-    # parse input into a dictionary
-    input_dict = {}
+    split_samples = filter(None, input_.read().split(delimiter))
+
+    input_samples = []
+    for raw_sample in split_samples:
+        def find_match(label, match_group=catchall):
+            return get_first_match(re.search(base_pattern % (label, match_group),
+                                             raw_sample))
+
+        sample = {}
+        # extract all the data and dump it into a dictionary
+        raw_time = find_match("Time", '"([^"]*)"')
+        raw_buttons = find_match("wButtons", '\[([^]]+)\]')
+
+        sample['time'] = parse_time(raw_time)
+        sample['buttons'] = filter(None, raw_buttons.split(",")) if raw_buttons else None
+        sample[LT] = int(find_match(LT))
+        sample[RT] = int(find_match(RT))
+        sample[LA] = (int(find_match(LAX)),
+                      int(find_match(LAY)))
+        sample[RA] = (int(find_match(RAX)),
+                      int(find_match(RAY)))
+        input_samples.append(sample)
 
     input_.close()
-    return input_dict
+    return input_samples
 
 def compose_from_controller(input_dict):
     bpm = 120
-    score = Score("Call of Duty Sonified", bpm)
+    # score = Score("Call of Duty Sonified", bpm)
 
 
 
 
 def main():
-    pass
+    parse_controller_input("test_input.txt")
 
 
 
